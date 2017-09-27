112f23f1925c6c8c3fb0e42b68924215bc3fcc90
cabilangan112 <jassencabilangan@gmail.com>
2017-09-27 16:26:20 +0800

    my 3rd commit

--- catalog/models.py
+++ catalog/models.py
@@ -8,11 +8,10 @@ import uuid
 
 
 class Genre(models.Model):
-	name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+	name= models.CharField(max_length=150, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
 	
-
-def __str__(self):
-	return self.name
+	def __str__(self):
+		return self.genre_name
 
 class Book(models.Model):
 	title = models.CharField(max_length=200)
@@ -20,12 +19,13 @@ class Book(models.Model):
 	summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
 	isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 	genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+	
+	def __str__(self):
+		return self.title
+	
+	def get_absolute_url(self):
+		return reverse('book-detail', args=[str(self.id)])
 
-def __str__(self):
-	return self.title
-
-def get_absolute_url(self):
-	return reverse('book-detail', args=[str(self.id)])
 
 class BookInstance(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
@@ -45,18 +45,22 @@ class BookInstance(models.Model):
     class Meta:
         ordering = ["due_back"]
         
-def __str__(self):
-    return '%s (%s)' % (self.id,self.book.title)
+
+    def __str__(self):
+        return '%s (%s)' % (self.id,self.book.title)
+
 	
 class Author(models.Model):
-	first_name = models.CharField(max_length=100)
-	last_name = models.CharField(max_length=100)
-	date_of_birth = models.DateField(null=True, blank=True)
-	date_of_death = models.DateField('Died', null=True, blank=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
 
-def get_absolute_url(self):
-	return reverse('author-detail', args=[str(self.id)])
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
 
-def __str__(self):
-	return '%s, %s' % (self.last_name, self.first_name)
+    def __str__(self):
+        return '%s, %s' % (self.last_name, self.first_name)
 
+    class Meta:
+        ordering = ['last_name']
